ab59a5aa4534057d85f5851485a709f788bed5f6
Makgul <113055548+AkgulMuhammed@users.noreply.github.com>
2022-10-10 17:00:12 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lime,
       ),
       debugShowCheckedModeBanner: false,
-      home: const    hotelHomePage(),
+      home: const    HotelStartPage(),
     );
   }
 }
